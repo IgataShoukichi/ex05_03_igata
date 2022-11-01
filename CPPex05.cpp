566e79ccd5dccc7619b2f4515d8801e830298880
igatashoukichi@gmail.com <igatashoukichi@gmail.com>
2022-11-01 17:23:31 +0900

    内容変更

--- CPPex05.cpp
+++ CPPex05.cpp
@@ -1,20 +1,7 @@
 #include <iostream>
+#include "getVolumeSurface.h"
 using namespace std;
-//#include "getVolumeSurface.h"
 
-struct BOX {	//” ‚Ì’è‹`
-	double x;	//•
-	double y;	//‚‚³
-	double z;	//‰œs
-	double volume;	//‘ÌÏ
-	double surface; //•\–ÊÏ
-};
-
-void getVolumeSurface(BOX* box) {
-	box->volume = box->x * box->y * box->z;
-	box->surface = 2 * (box->x * box->y + box->x * box->z + box->y * box->z);
-	return;
-}
 int main()
 {
 	const int BoxCount = 3;
